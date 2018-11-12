97b8ec752456c8ea9793d8c124bc2b88318b50a3
Aristeu Roriz <aristeuroriz@gmail.com>
2018-11-12 19:48:21 -0200

    try publishing game 1

---
+++ docs/precache-manifest.2d44f60032c411a83d884d236874e0a6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a138c74e9cc21761917da5e8c42262a",
+    "url": "/static/media/virus.1a138c74.png"
+  },
+  {
+    "revision": "f02dc31c363e647739a141c0762888f3",
+    "url": "/static/media/virus-61-493635.f02dc31c.png"
+  },
+  {
+    "revision": "1383dd3552622bc5d13ec2e1e1b8645e",
+    "url": "/static/media/bacteria-png-8.1383dd35.png"
+  },
+  {
+    "revision": "d0a7bee68cf6982b0fbbdba12d4c0b74",
+    "url": "/static/media/bacteria-png-6.d0a7bee6.png"
+  },
+  {
+    "revision": "6aa017c54ca7da0494fcbc332189e665",
+    "url": "/static/media/Virus.6aa017c5.ico"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "95b0f0b4f3bbfee0e8c1",
+    "url": "/static/js/main.95b0f0b4.chunk.js"
+  },
+  {
+    "revision": "219599ea11ec00b6f308",
+    "url": "/static/js/1.219599ea.chunk.js"
+  },
+  {
+    "revision": "95b0f0b4f3bbfee0e8c1",
+    "url": "/static/css/main.8f3a1b20.chunk.css"
+  },
+  {
+    "revision": "9b1c8c795ba8494591c724559e01ec99",
+    "url": "/index.html"
+  }
+];
