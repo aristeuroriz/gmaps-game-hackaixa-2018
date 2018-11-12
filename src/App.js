d0e91505fd6c115695ae8f25ec38c60c90dc9041
Aristeu Roriz Neto <aristeu.neto@caixa.gov.br>
2018-11-12 10:34:15 -0200

    layout ok

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import * as maps from "@google/maps";
+import { Navbar, Grid, Row, Col } from "react-bootstrap";
 class App extends Component {
   constructor(props) {
     super(props);
@@ -7,11 +7,6 @@ class App extends Component {
   }
   componentDidMount() {
     this.initMap();
-    window.maps = maps;
-    let googleMapsClient = maps.createClient({
-      key: "AIzaSyA8YTM7M3Bym1Poicd0E3lCFxlc9UFKpaU"
-    });
-    window.googleMapsClient = googleMapsClient;
   }
 
   initMap() {
@@ -23,8 +18,45 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="App">
-        <div id="map" style={{ width: "100%", height: "800px" }} />
+      <div className="container-fluid">
+        <Navbar
+          className="navbar-fixed-top"
+          style={{
+            backgroundImage:
+              "linear-gradient(80deg,rgb(0, 92, 169) 30%,rgb(84, 187, 171))",
+            border: "0px",
+            height: "80px"
+          }}
+        >
+          <Navbar.Header>
+            <Navbar.Brand>
+              <h1 style={{ color: "white" }}>GMaps Game - Hackaixa</h1>
+            </Navbar.Brand>
+          </Navbar.Header>
+          {/* <Nav>
+            <NavItem eventKey={1} href="#">
+              Link
+            </NavItem>
+            <NavItem eventKey={2} href="#">
+              Link
+            </NavItem>
+            <NavDropdown
+              className="pull-right"
+              eventKey={3}
+              title="Dropdown"
+              id="basic-nav-dropdown"
+            >
+              <MenuItem eventKey={3.1}>Action</MenuItem>
+            </NavDropdown>
+          </Nav> */}
+        </Navbar>
+        <Grid>
+          <Row>
+            <Col>
+              <div id="map" style={{ width: "100%", height: "600px" }} />
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
